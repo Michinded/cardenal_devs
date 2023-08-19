a481b01d288f03f7ccd530f03845c71e626da908
Michinded <michinded@gmail.com>
2023-08-19 12:19:30 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:cardenal_devs/pages/login/login_controller.dart';
 import 'package:cardenal_devs/pages/login/login_view.dart';
+import 'package:cardenal_devs/pages/test/test_firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
