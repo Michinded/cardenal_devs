bddf1d5dfedb61c77a8117575dbb62a2ccd37458
Michinded <michinded@gmail.com>
2023-08-19 14:02:18 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:cardenal_devs/pages/login/login_controller.dart';
 import 'package:cardenal_devs/pages/login/login_view.dart';
 import 'package:cardenal_devs/pages/test/test_firebase_auth.dart';
 import 'package:cardenal_devs/pages/register/register_view.dart';
@@ -11,9 +10,8 @@ import 'package:firebase_core/firebase_core.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
-void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
