1a142252b42f6b50055837a6fcfbed918ddc0eb6
Michinded <michinded@gmail.com>
2023-08-19 12:18:33 -0600

    Update auth_provider.dart

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,25 +1,29 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
+// Esta clase se encarga de la autenticación de los usuarios a través de Firebase Auth
+
 class AuthProvider{
+  // Se crea una instancia de FirebaseAuth
   late FirebaseAuth _firebaseAuth;
   AuthProvider(){
     _firebaseAuth = FirebaseAuth.instance;
   }
-
+  // Se crea el método login que recibe como parámetros el email y el password
   Future<bool> login (String email, String password) async{
     String? errorMessage;
     try{
       await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
       return true;
     }catch(error) {
-      print(error);
+     // print(error); // Imprime el error en la consola de debug
       errorMessage = error.hashCode as String?;
     }
 
     if(errorMessage != null){
-      return Future.error(errorMessage);
+      //return Future.error(errorMessage); // Regresa el error en caso de que exista
+      return false; // Regresa false en caso de que no exista el usuario exista
     }
-    return true;
+    return true; // Regresa true en caso de que el usuario exista
 
   }
 
